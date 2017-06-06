f37dd922567084aab6e5c7cd06fe5dfbd8b55a9f
秦守丽 <qsl0705@163.com>
2017-06-06 12:50:08 +0800

    modify seed file add deletion to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Category.delete_all
+Product.delete_all
+Order.delete_all
+Cart.delete_all
+CartItem.delete_all
+
 u = User.new
 u.email = "qsl0705@163.com"
 
